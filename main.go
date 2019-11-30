c3acb8554e2f62fc7ff8569a1ef1bda716ba8d20
brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-11-29 21:30:00 -0300

    Couple more.

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"strconv"
 )
+
+func matrix(p [][]int, n int, m int) [][]int {
+	p = make([][]int, n)
+	for i := 0; i < n; i++ {
+		p[i] = make([]int, m)
+	}
+	return p
+}
+
+func remove(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
 
 // Write a function that returns the sum of two numbers.
 
@@ -51,6 +65,7 @@ func centuryFromYear(year int) int {
 // For inputArray = [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
 
 // 7 and 3 produce the largest product.
+
 // a := [...]int{3, 6, -2, -5, 7, 3}
 // adjacentElementsProduct(a[:])
 
@@ -80,9 +95,6 @@ func adjacentElementsProduct(slice []int) int {
 
 //	a := [...]int{1, 3, 2, 3, 3}
 // print(almostIncreasingSequence(a[:]))
-func remove(slice []int, s int) []int {
-	return append(slice[:s], slice[s+1:]...)
-}
 
 func isSequence(slice []int, index int) bool {
 	aux := remove(slice[:], index)
@@ -121,6 +133,113 @@ func almostIncreasingSequence(slice []int) bool {
 
 // Ratiorg needs statues of sizes 4, 5 and 7.
 
+//a := [...]int{1, 3, 2, 5}
+// print(makeArrayConsecutive2(a[:]))
+
+func makeArrayConsecutive2(slice []int) int {
+	sort.Ints(slice)
+	c := 0
+	for i := 1; i < len(slice)-1; i++ {
+		if slice[i+1]-slice[i] > 1 {
+			c += slice[i+1] - slice[i] - 1
+		}
+	}
+	return c
+}
+
+// Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+// A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+
+// Example
+
+// For n = 2, the output should be shapeArea(n) = 5; For n = 3, the output should be shapeArea(n) = 13.
+
+func shapeArea(n int) int {
+	if n == 1 {
+		return n
+	}
+	acc := 1
+	for i := 1; i <= n; i++ {
+		acc += 4 * i
+	}
+	return acc
+}
+
+// After becoming famous, the CodeBots decided to move into a new building together. Each of the rooms has a different cost, and some of them are free, but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+
+// Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, your task is to return the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that don't appear below a 0).
+
+// Example
+
+// For
+
+// matrix = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]] the output should be matrixElementsSum(matrix) = 9.
+
+// example 1
+
+// There are several haunted rooms, so we'll disregard them as well as any rooms beneath them. Thus, the answer is 1 + 5 + 1 + 2 = 9.
+
+// For
+
+// matrix = [[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]] the output should be matrixElementsSum(matrix) = 9.
+
+// example 2
+
+// Note that the free room in the final column makes the full column unsuitable for bots (not just the room directly beneath it). Thus, the answer is 1 + 1 + 1 + 5 + 1 = 9.
+
+// function matrixes(arr) {
+// 	let sum = 0;
+// 	for (let i = 1; i < arr.length; i++) {
+// 		for (let j = 0; j < arr[i].length; j++) {
+// 			console.log(arr);
+// 			if (!arr[i - 1][j]) arr[i][j] = 0;
+// 		}
+// 	}
+// 	for (let i = 0; i < arr.length; i++) {
+// 		arr[i] = arr[i].filter(k => k);
+// 	}
+// 	arr.map(k => k.map(w => (sum += w)));
+// 	console.log(arr, sum);
+// 	return sum;
+// }
+
+func matrixElementsSum(slice [][]int) int {
+	var sum int = 0
+	for i := 1; i < len(slice); i++ {
+		for j := 0; j < len(slice[i]); j++ {
+			fmt.Println(slice)
+			if slice[i-1][j] != 0 {
+				slice[i][j] = 0
+			}
+		}
+	}
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice); j++ {
+			if slice[i][j] == 0 {
+				slice[i] = remove(slice[i], j)
+			}
+		}
+	}
+	fmt.Println(slice)
+
+	return sum
+
+}
+
+// [[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]]
 func main() {
+	// a := make([][]int, 0)
+	// var m, n int
+	// var mat = make([][]int, m)
+	// for i := range mat {
+	// 	mat[i] = make([]int, n)
+	// }
+	var a = [...][]int{
+		{1, 1, 1, 0},
+		{0, 5, 0, 1},
+		{2, 1, 3, 10},
+	}
+	print(matrixElementsSum(a[:][:]))
 
 }
